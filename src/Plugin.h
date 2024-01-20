01521a3f9e7bf1e321fb1cb2a7dda531ea3e736f
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-01-20 10:23:48 +0300

    Update Plugin.h

--- src/Plugin.h
+++ src/Plugin.h
@@ -1,6 +1,42 @@
 #pragma once
 
 #include <ll/api/plugin/NativePlugin.h>
+#include <ll/api/dimension/CustomDimensionManager.h>
+
+class SkyBlock  : public Dimension
+{
+public:
+    LLNDAPI SkyBlock(std::string const& name, DimensionFactoryInfo const& info);
+
+    LLNDAPI static CompoundTag generateNewData(uint seed = 123, GeneratorType generatorType = GeneratorType::Void);
+    
+    void init() override;
+
+    std::unique_ptr<WorldGenerator> createGenerator() override;
+
+    void upgradeLevelChunk(ChunkSource& chunkSource, LevelChunk& oldLc, LevelChunk& newLc) override;
+
+    void fixWallChunk(ChunkSource& cs, LevelChunk& lc) override;
+
+    bool levelChunkNeedsUpgrade(LevelChunk const& lc) const override;
+
+    void _upgradeOldLimboEntity(CompoundTag& tag, ::LimboEntitiesVersion vers) override;
+
+    Vec3 translatePosAcrossDimension(Vec3 const& pos, DimensionType did) const override;
+
+    std::unique_ptr<ChunkSource>
+    _wrapStorageForVersionCompatibility(std::unique_ptr<ChunkSource> cs, ::StorageVersion ver) override;
+
+
+    mce::Color getBrightnessDependentFogColor(mce::Color const& color, float brightness) const override;
+
+    short getCloudHeight() const override;
+
+    bool hasPrecipitationFog() const override;
+
+    std::unique_ptr<StructureFeatureRegistry>
+    makeStructureFeatures(bool isLegacy, BaseGameVersion const& baseGameVersion, Experiments const& experiments);
+};
 
 namespace plugin {
 
