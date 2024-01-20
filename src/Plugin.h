1840fd8037d208d2da87b60ffb1be17269149299
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-01-20 17:37:13 +0300

    Update Plugin.h

--- src/Plugin.h
+++ src/Plugin.h
@@ -7,6 +7,58 @@
 #include <mc/world/level/dimension/NetherBrightnessRamp.h>
 #include <mc/world/level/dimension/OverworldBrightnessRamp.h>
 #include <mc/world/level/levelgen/structure/StructureFeatureRegistry.h>
+#include <ll/api/utils/RandomUtils.h>
+
+#include <mc/common/wrapper/FeatureRefTraits.h>
+#include <mc/common/wrapper/WeakRefT.h>
+#include <mc/deps/core/debug/log/ContentLog.h>
+#include <mc/deps/core/string/HashedString.h>
+#include <mc/deps/core/utility/buffer_span.h>
+#include <mc/deps/json/Reader.h>
+#include <mc/deps/json/Value.h>
+#include <mc/deps/json/Writer.h>
+#include <mc/util/Random.h>
+#include <mc/util/random/XoroshiroPositionalRandomFactory.h>
+#include <mc/world/level/BlockPalette.h>
+#include <mc/world/level/BlockSource.h>
+#include <mc/world/level/ChunkBlockPos.h>
+#include <mc/world/level/ChunkPos.h>
+#include <mc/world/level/Level.h>
+#include <mc/world/level/biome/Biome.h>
+#include <mc/world/level/biome/registry/BiomeRegistry.h>
+#include <mc/world/level/biome/source/BiomeArea.h>
+#include <mc/world/level/biome/source/FixedBiomeSource.h>
+#include <mc/world/level/block/Block.h>
+#include <mc/world/level/block/BlockVolume.h>
+#include <mc/world/level/block/registry/BlockTypeRegistry.h>
+#include <mc/world/level/block/utils/VanillaBlockTypeIds.h>
+#include <mc/world/level/chunk/ChunkViewSource.h>
+#include <mc/world/level/chunk/LevelChunk.h>
+#include <mc/world/level/dimension/Dimension.h>
+#include <mc/world/level/dimension/OverworldBrightnessRamp.h>
+#include <mc/world/level/levelgen/WorldGenerator.h>
+#include <mc/world/level/levelgen/feature/BambooFeature.h>
+#include <mc/world/level/levelgen/feature/FlowerFeature.h>
+#include <mc/world/level/levelgen/feature/LakeFeature.h>
+#include <mc/world/level/levelgen/feature/registry/FeatureRegistry.h>
+#include <mc/world/level/levelgen/flat/FlatWorldGenerator.h>
+#include <mc/world/level/levelgen/flat/FlatWorldGeneratorOptions.h>
+#include <mc/world/level/levelgen/structure/StructureFeatureRegistry.h>
+#include <mc/world/level/levelgen/structure/StructureFeatureType.h>
+#include <mc/world/level/levelgen/v1/Aquifer.h>
+#include <mc/world/level/levelgen/v1/NetherGenerator.h>
+#include <mc/world/level/levelgen/v1/OverworldGenerator2d.h>
+#include <mc/world/level/levelgen/v1/OverworldGeneratorMultinoise.h>
+#include <mc/world/level/levelgen/v1/WorldGenCache.h>
+#include <mc/world/level/storage/LevelData.h>
+
+class IslandGenerator : public VoidGenerator
+{
+public:
+   IslandGenerator(Dimension& dimension) : VoidGenerator(dimension) {}
+   ~ IslandGenerator() {}
+   virtual bool postProcess(ChunkViewSource& chunkViewSource);
+};
 
 namespace ll::dimension
 {
